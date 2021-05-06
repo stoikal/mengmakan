ffec1981147235c5512d89d28069a282343c5b95
Laksito Pamilih <laksito@pm.me>
2021-05-06 10:49:40 +0700

    feat(main content): style restaurant card

--- src/scripts/components/RestaurantCard/index.js
+++ src/scripts/components/RestaurantCard/index.js
@@ -6,7 +6,7 @@ const truncate = (str, maxLength) => {
   if (length <= maxLength) {
     return str
   } else {
-    return str.slice(0, maxLength) + '...'
+    return str.slice(0, maxLength).trim() + '...'
   }
 }
 
@@ -24,13 +24,13 @@ export default class RestaurantCard {
     card.className = styles.container;
     card.innerHTML = `
       <div class=${styles.thumbWrapper}>
-  
+        <img src=${imgSrc} alt="penampakan restoran"/>
         <span>${city}</span>
       </div>
-      <div>
+      <div class=${styles.descriptionWrapper}>
         <span>Rating: ${rating}</span>
-        <p>${name}</p>
-        <p>${truncate(description, 300)}</p>
+        <p><b>${name}</b></p>
+        <p>${truncate(description, 150)}</p>
       </div>
     `
     return card;
