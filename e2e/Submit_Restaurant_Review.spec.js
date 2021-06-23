a385de405b2715b23b5f01ea9be208e596471889
Laksito Pamilih <laksito@pm.me>
2021-06-23 09:50:31 +0700

    remove duplicate scenario in e2e

--- e2e/Submit_Restaurant_Review.spec.js
+++ e2e/Submit_Restaurant_Review.spec.js
@@ -41,17 +41,3 @@ Scenario('submitting new review', async ({ I }) => {
     I.see(review, { css: 'div[name="review-item"] p' });
   });
 });
-
-Scenario('seeing restaurant menu', ({ I }) => {
-  I.amOnPage('/');
-  I.seeElement('restaurant-card');
-
-  I.click('pierce/.description-wrapper a.name'); // click first restaurant in list
-
-  I.seeElement('pierce/button.tab-title');
-  I.see('Menu', 'pierce/button.tab-title:last-child');
-
-  I.click('pierce/button.tab-title:last-child');
-  I.see('Makanan', 'tab-content span b');
-  I.see('Minuman', 'tab-content span b');
-});
